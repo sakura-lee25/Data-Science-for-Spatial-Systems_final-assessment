f9d5cde280eb0a93551547d5ed0bc695bc86de29
sakura-lee25 <ucfnql2@ucl.ac.uk>
2026-04-16 13:41:59 +0100

    Update ml_classification.py

--- src/analysis/ml_classification.py
+++ src/analysis/ml_classification.py
@@ -30,9 +30,7 @@ from src.utils.config import (
 # Features used for classification
 FEATURE_COLS = [
     "imd_score",
-    "junction_density",
-    "road_density",
-    "urban_pct",
+    'infra_index',
     "dark_pct",
     "wet_road_pct",
     "road_length_km",
